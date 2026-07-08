1c3963fb793a658ff25669795e47bb266ae59873
Vinicus Levi <viniciushys32@gmail.com>
2026-07-07 23:34:44 -0300

    Ficou bom

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let MaximoLimite = 50;
+let MaximoLimite = 60;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 60');
 }
 
 exibirMensagemInicial();
